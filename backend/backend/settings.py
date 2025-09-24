a2a2e8ef2edb12c169abaabf2b01fe1bca79e6d1
Asim <asimokon@yahoo.com>
2025-09-24 13:45:06 +0100

    regiistration flow

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hidden',
 ]
 
 MIDDLEWARE = [
@@ -72,10 +73,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hidden_note_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -120,3 +132,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "hidden.User"
--- backend/hidden/models.py
+++ backend/hidden/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.text import slugify
+import uuid
 
-# Create your models here.
+class User(AbstractUser):
+    class RoleChoices(models.TextChoices):
+        USER = "user"
+        ADMIN = "admin"
+ 
+
+    role = models.CharField(max_length=10, choices=RoleChoices.choices, default=RoleChoices.USER)
+    secret_link = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.secret_link:
+            # Create slug with username + part of uuid
+            unique_id = str(uuid.uuid4())[:8]
+            self.secret_link = slugify(f"{self.username}-{unique_id}")
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messages")
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Message to {self.user.username} at {self.created_at}"
+
