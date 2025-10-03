e341ab0a5f6e38ef7e5486b7b4a3ffa9ff97bc2c
Asim <asimokon@yahoo.com>
2025-10-03 22:49:27 +0100

    generic view for getting all data

--- backend/hidden/urls.py
+++ backend/hidden/urls.py
@@ -3,12 +3,12 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 urlpatterns = [
-    path("register/", views.register, name="register"),
+    path("register/", views.RegisterView.as_view(), name="register"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("message/send/<str:secret_link>/",
-         views.send_message, name="send_message"),
+         views.SendMessageView.as_view(), name="send_message"),
     path("messages/", views.MessageListView.as_view(), name="messages"),
-    path("message/delete/<int:id>/", views.delete_message, name="delete_message"),
+    path("message/<int:id>/", views.GetAndDeleteMessageView.as_view(), name="get_delete_message"),
     path("users/", views.UserListView.as_view(), name="users"),
 ]
--- backend/hidden/views.py
+++ backend/hidden/views.py
@@ -1,6 +1,7 @@
 from hidden.serializers import RegisterSerializer, UserSerializer, MessageSerializer
 from rest_framework import status
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
@@ -12,14 +13,18 @@ from rest_framework import generics
 User = get_user_model()
 
 
-@api_view(["POST"])
-@permission_classes([AllowAny])  
-def register(request):
-    serializer = RegisterSerializer(data=request.data)
-    if serializer.is_valid():
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        # serialize user to return safe info
+
+        # return safe user data
         user_data = UserSerializer(user).data
+
         return Response(
             {
                 "message": "User registered successfully",
@@ -27,22 +32,47 @@ def register(request):
             },
             status=status.HTTP_201_CREATED
         )
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(["POST"])
-@permission_classes([AllowAny])
-def send_message(request, secret_link):
-    try:
-        user = User.objects.get(secret_link=secret_link)
-    except User.DoesNotExist:
-        return error_response("Invalid link", code="invalid_link", status_code=status.HTTP_404_NOT_FOUND)
-    serializer = MessageSerializer(data=request.data)
-    if serializer.is_valid():
+# @api_view(["POST"])
+# @permission_classes([AllowAny])  
+# def register(request):
+#     serializer = RegisterSerializer(data=request.data)
+#     if serializer.is_valid():
+#         user = serializer.save()
+#         # serialize user to return safe info
+#         user_data = UserSerializer(user).data
+#         return Response(
+#             {
+#                 "message": "User registered successfully",
+#                 "user": user_data
+#             },
+#             status=status.HTTP_201_CREATED
+#         )
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# @api_view(["POST"])
+# @permission_classes([AllowAny])
+# def send_message(request, secret_link):
+#     try:
+#         user = User.objects.get(secret_link=secret_link)
+#     except User.DoesNotExist:
+#         return error_response("Invalid link", code="invalid_link", status_code=status.HTTP_404_NOT_FOUND)
+#     serializer = MessageSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save(user=user)
+#         return Response({"message": "Message sent successfully"}, status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SendMessageView(generics.CreateAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        secret_link = self.kwargs.get("secret_link")
+        user = get_object_or_404(User, secret_link=secret_link)
         serializer.save(user=user)
-        return Response({"message": "Message sent successfully"}, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 # @api_view(["GET"])
 # @permission_classes([IsAuthenticated])
@@ -60,17 +90,17 @@ class MessageListView(generics.ListAPIView):
         return Message.objects.filter(user=self.request.user)
 
 
-@api_view(["DELETE"])
-@permission_classes([IsAuthenticated])
-def delete_message(request, id):
-    try:
-        message = Message.objects.get(id=id, user=request.user)
-    except Message.DoesNotExist:
-        return error_response("", code="invalid_link", status_code=status.HTTP_404_NOT_FOUND)
-        # return Response({"error": "Message not found"}, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(["DELETE"])
+# @permission_classes([IsAuthenticated])
+# def delete_message(request, id):
+#     try:
+#         message = Message.objects.get(id=id, user=request.user)
+#     except Message.DoesNotExist:
+#         return error_response("", code="invalid_link", status_code=status.HTTP_404_NOT_FOUND)
+#         # return Response({"error": "Message not found"}, status=status.HTTP_400_BAD_REQUEST)
 
-    message.delete()
-    return Response({"message": "Deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+#     message.delete()
+#     return Response({"message": "Deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
 
 
 # @api_view(["GET"])
@@ -80,10 +110,18 @@ def delete_message(request, id):
 #     serializer = UserSerializer(users, many=True)
 #     return Response(serializer.data)
 
+class GetAndDeleteMessageView(generics.RetrieveDestroyAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_url_kwarg = 'id'
+
+    def get_queryset(self):
+        return Message.objects.filter(user=self.request.user)
+
 
 class UserListView(generics.ListAPIView):
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated, IsAdminUser]
+    permission_classes = [AllowAny]
 
     def get_queryset(self):
         return User.objects.all()
