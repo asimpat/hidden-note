68fc91ee832546d652cd0b4d29a5ea51c9bbdf6e
Asim <asimokon@yahoo.com>
2025-10-06 22:29:27 +0100

    explored rate limiting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wgyarvhx((0q6ppe^w42u2ly*)i)ovc+hnymlkt%!tas#2yamv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True 
 
 ALLOWED_HOSTS = []
 
