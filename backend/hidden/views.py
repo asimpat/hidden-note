7757fe9eb497b9d272a3ced232875561152a7301
Asim <asimokon@yahoo.com>
2025-10-10 21:56:57 +0100

    message list

--- backend/hidden/views.py
+++ backend/hidden/views.py
@@ -116,7 +116,7 @@ class MessageListView(generics.ListAPIView):
 #     return Response({"message": "Deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
 
 
-# @api_view(["GET"]) 
+# @api_view(["GET"])  
 # @permission_classes([AllowAny])
 # def get_users(request):
 #     users = User.objects.all()
